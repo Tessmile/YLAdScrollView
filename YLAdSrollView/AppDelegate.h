f4e543ab2e59fa511dec8e3fb6acb3770a0c0cfa
鲁玉兰 <luyulan@luyulandemacbook-pro.local>
2016-06-15 15:30:23 +0800

    Initial Commit

---
+++ YLAdSrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLAdSrollView
+//
+//  Created by 鲁玉兰 on 16/6/15.
+//  Copyright © 2016年 luyulan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
